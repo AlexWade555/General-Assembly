22eb6efa98a58d3b490b6b98142821d94ef36abb
Alex Wade <alexjohnwade@gmail.com>
2023-01-02 16:54:49 +1100

    moved props to a variable

--- 01-hello-world/src/index.js
+++ 01-hello-world/src/index.js
@@ -2,7 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Hello from './App.js';
 
+var person = {
+  personName: "Nick",
+  personAge: 24
+}
+
 ReactDOM.render(
-  <Hello name={"Nick"} age={24} />,
+  <Hello
+  name={person.personName}
+  age={person.personAge} />,
   document.getElementById('root')
 )
