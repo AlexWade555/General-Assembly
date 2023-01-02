509c9759560665cc6621e8668b7595bf7a192928
Alex Wade <alexjohnwade@gmail.com>
2023-01-02 16:49:17 +1100

    used the props in the app.js

--- 01-hello-world/src/App.js
+++ 01-hello-world/src/App.js
@@ -10,7 +10,7 @@ class Hello extends Component {
     // Make sure to return some UI.
     return (
       <div>
-        <h1>Hello World!</h1>
+        <h1>Hello {this.props.name}!</h1>
         <h3>It is time for tea.</h3>
       </div>
     )
