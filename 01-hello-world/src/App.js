04d56438acf7c2345f669bfbfb405122adc37918
Alex Wade <alexjohnwade@gmail.com>
2023-01-02 16:51:33 +1100

    added name and used it

--- 01-hello-world/src/App.js
+++ 01-hello-world/src/App.js
@@ -11,7 +11,7 @@ class Hello extends Component {
     return (
       <div>
         <h1>Hello {this.props.name}!</h1>
-        <h3>It is time for tea.</h3>
+        <p>You ae {this.props.age} years old.</p>
       </div>
     )
   }
