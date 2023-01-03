417c6cd5a12dd0efb9246bb1435d6edcc793f067
Alex Wade <alexjohnwade@gmail.com>
2023-01-03 11:54:34 +1100

    added var for data to pass

--- 02-blog-posts/02-blog-posts/src/index.js
+++ 02-blog-posts/02-blog-posts/src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Post from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+var examplePost = {
+  title: "Dinosaurs are awesome",
+  author: "Stealthy Stegosaurus",
+  body: "Check out this body property!",
+  comments: ["First!", "Great post", "Hire this author now!"]
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(
+  <Post />,
+  document.getElementById('root')
+)
