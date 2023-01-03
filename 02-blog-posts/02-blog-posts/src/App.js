7eae53ac0669adadf7a2c76b569eec2077cc4b1b
Alex Wade <alexjohnwade@gmail.com>
2023-01-03 12:16:43 +1100

    added all the data

--- 02-blog-posts/02-blog-posts/src/App.js
+++ 02-blog-posts/02-blog-posts/src/App.js
@@ -1,11 +1,17 @@
-import './App.css';
+import React, {Component} from 'react';
 
-function Post() {
-  return (
-    <div>
-
-    </div>
-  );
+class Post extends Component {
+  render () {
+    return (
+      <div>
+        <h1> {this.props.title}</h1>
+        <h2>by {this.props.author}</h2>
+        <p>{this.props.body}</p>
+        <h3>Comments:</h3>
+        <p>{this.props.comments[0]}</p>
+      </div>
+    )
+  }
 }
 
-export default Post;
+export default Post
